55c1d539e4fb0881af2b4c1c190bd873035cd027
Mehman Osmanov <mehmanosmanof@gmail.com>
2023-03-04 11:23:15 +0400

    last

--- src/listOfStudentApp/enums/SortFields.java
+++ src/listOfStudentApp/enums/SortFields.java
@@ -1,6 +1,6 @@
 package listOfStudentApp.enums;
 
 public enum SortFields {
-    NAME, SURNAME, AGE, SCORE, ID, BIRTHDATE, ADMISSIONDATE,UNKNOWN;
+    NAME, SURNAME, AGE, SCORE, ID, BIRTHDATE, ADMISSIONDATE;
 }
 
