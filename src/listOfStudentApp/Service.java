c6fa888e18321bc2bc650d05b847c366277331ca
Mehman Osmanov <mehmanosmanof@gmail.com>
2023-04-03 02:36:00 +0400

    last update

--- src/listOfStudentApp/Service.java
+++ src/listOfStudentApp/Service.java
@@ -42,7 +42,7 @@ public final class Service {
                     break;
                 case "2":
                     try {
-                        Comparator<Student> comparator = SortingByField.sortByField(students);
+                        Comparator<Student> comparator = SortingByField.sortByField();
                         students.sort(comparator);
                         ShowingStudents.showStudents();
                     } catch (IllegalArgumentException ex) {
